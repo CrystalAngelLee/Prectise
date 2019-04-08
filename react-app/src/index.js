cf7e93458059f176cccf693fad554d45b8dfa7fd
crystal <crystaljiaxuan@163.com>
2019-04-08 18:01:02 +0800

    整理

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import BackToTop from './back-to-top';
-import BackTopDemo from './test/back-to-top';
+import BackTopDemo from './demo/back-to-top';
+import './style'
 
 ReactDOM.render(<BackTopDemo /> , document.getElementById('root'));
