3cb7709d07708a49e4ae3f1ace9b2f2a1ab13cc9
crystal <crystaljiaxuan@163.com>
2020-09-21 17:13:51 +0800

    commit

---
+++ react-app/src/demo/scrollbar/Iframe.js
@@ -0,0 +1,38 @@
+import React, { PureComponent } from "react";
+
+const image = require("../../statics/大图_.jpg");
+
+const Content = `
+  <div id="wea_rich_text_default_font" style="font-size:12px;">
+    <p>
+      &nbsp;
+      <img alt="" src="${image}" class="formImgPlay" />
+      &nbsp;
+    </p>
+  </div>`;
+
+export default class IframeComp extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.iframe = React.createRef();
+  }
+
+  componentDidMount() {
+    this.setContent(Content);
+  }
+
+  setContent = value => {
+    const frame = this.iframe.current;
+    const win = frame.contentWindow;
+    const doc = win.document;
+    console.log(doc);
+    if (typeof value === "string") {
+      doc.write('<html><head><style>table{border-collapse: collapse;}</style></head><body style="overflow-y:hidden"></body></html>');
+      doc.body.innerHTML = value;
+    }
+  };
+
+  render() {
+    return <iframe src="javascript:document.open();document.close();" ref={this.iframe} title="testScroll" height="100%" width="500" />;
+  }
+}
--- react-app/src/demo/scrollbar/index.js
+++ react-app/src/demo/scrollbar/index.js
@@ -1,23 +1,38 @@
-import React, { Component } from 'react'
-import './index.css'
-import ScrollBar from '../../components/scrollbar'
+import React, { Component } from "react";
+import { Tabs } from "antd";
+import "./index.css";
+import ScrollBar from "../../components/scrollbar";
+import Iframe from "./Iframe";
+
+const { TabPane } = Tabs;
 
 export default class ScrollBarTest extends Component {
   render() {
     return (
-      <div>
+      <div className="scrolltest">
         <h3>横向滚动条</h3>
-        <div className='container'>
-          <div className="scrollContainer" id='scrollContainer'>
-            <div className='article' style={{ width: 400 }}>
-              The indefinite article takes two forms. It’s the word a when it precedes a word that begins with a consonant. It’s the word an when it precedes a word that begins with a vowel. The indefinite article indicates that a noun refers to a general idea rather than a particular thing. For example, you might ask your friend, “Should I bring a gift to the party?” Your friend will understand that you are not asking about a specific type of gift or a specific item. “I am going to bring an apple pie,” your friend tells you. Again, the indefinite article indicates that she is not talking about a specific apple pie. Your friend probably doesn’t even have any pie yet. The indefinite article only appears with singular nouns. Consider the following examples of indefinite articles used in context:
-            </div>
-          </div>
-          <ScrollBar
-            targetDom={'#scrollContainer'}
-          />
+
+        <div className="scrolltest-container">
+          <Tabs defaultActiveKey="1">
+            <TabPane tab="Tab 1" key="1">
+              <div className="div-container">
+                <div className="scrollContainer" id="scrollContainer">
+                  <div className="article" style={{ width: 400 }}>
+                    The indefinite article takes two forms. It’s the word a when it precedes a word that begins with a consonant. It’s the word an when it precedes a word that begins with a vowel. The indefinite article indicates that a noun refers to a general idea rather than a particular thing. For example, you might ask your friend, “Should I bring a gift to the party?” Your friend will understand that you are not asking about a specific type of gift or a specific item. “I am going to bring an apple pie,” your friend tells you. Again, the indefinite article indicates that she is not talking about a specific apple pie. Your friend probably doesn’t even have any pie yet. The indefinite article only appears with singular nouns. Consider the following examples of indefinite articles used
+                    in context:
+                  </div>
+                </div>
+                <ScrollBar targetDom={"#scrollContainer"} />
+              </div>
+            </TabPane>
+            <TabPane tab="Tab 2" key="2">
+              <div className="iframe-container">
+                <Iframe />
+              </div>
+            </TabPane>
+          </Tabs>
         </div>
       </div>
-    )
+    );
   }
 }
